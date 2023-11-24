2704ce719e8deb4a30f53cfc094357198abd0fc9
noob-bholu <143063747+Noob-Bholu@users.noreply.github.com>
2023-11-24 18:53:57 +0530

    Update locks_sql.py

--- NoobBholu/modules/sql/locks_sql.py
+++ NoobBholu/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from tg_bot.modules.sql import SESSION, BASE
+from NoobBholu.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
