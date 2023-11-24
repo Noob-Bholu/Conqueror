92e52ec8421a6201bf9be0cf563156b44ec4f8fa
noob-bholu <143063747+Noob-Bholu@users.noreply.github.com>
2023-11-24 18:58:07 +0530

    Update rules_sql.py

--- NoobBholu/modules/sql/rules_sql.py
+++ NoobBholu/modules/sql/rules_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from tg_bot.modules.sql import SESSION, BASE
+from NoobBholu.modules.sql import SESSION, BASE
 
 
 class Rules(BASE):
